06aa31b15558c6460d96249891c1b31c03259e4b
guangyuw <esfak47@qq.com>
2016-11-28 13:36:31 +0800

    git add setup nginx

---
+++ ppmessage/scripts/setupnginxondebianorubuntu.sh
@@ -0,0 +1,11 @@
+NGINX_VERSION=1.8.0
+
+cd /tmp \
+&& wget http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz \
+&& git clone https://github.com/vkholodkov/nginx-upload-module.git \
+&& cd nginx-upload-module && git checkout 2.2 && cd ../ \
+&& tar -xzvf nginx-$NGINX_VERSION.tar.gz \
+&& cd nginx-$NGINX_VERSION \
+&& ./configure --with-http_ssl_module \
+--add-module=../nginx-upload-module \
+&& make && make install
